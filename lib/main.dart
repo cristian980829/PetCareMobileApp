a4702ec94c11beadc7bc4f5b64700ee45f8bd1bd
Camilo Jaramillo <camilojd77@gmail.com>
2021-10-20 21:35:06 -0500

    Begin login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pet_care_app/screen/login_screen.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PetCare App',
+      home: LoginScreen(),
+    );
+  }
+}
